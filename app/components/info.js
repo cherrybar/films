e9ed64d4627e8adfb36400b782d70bf2427f3ce9
cherrybar <elenal@kmslh.com>
2021-04-09 18:04:26 +0300

    add info component

---
+++ app/components/info.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+export default class InfoComponent extends Component {
+  get posterStyles() {
+    let {movie} = this.args
+    return `background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6647701316854867) 35%, rgba(255,255,255,0) 100%),
+    url(https://image.tmdb.org/t/p/w500${movie.poster_path})`
+  }
+}
+

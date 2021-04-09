6591a7912356e15d7dd05f3bd2d146634d5c2018
cherrybar <elenal@kmslh.com>
2021-04-09 18:45:52 +0300

    fix genres number

--- app/components/info.js
+++ app/components/info.js
@@ -6,5 +6,10 @@ export default class InfoComponent extends Component {
     return `background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6647701316854867) 35%, rgba(255,255,255,0) 100%),
     url(https://image.tmdb.org/t/p/w500${movie.poster_path})`
   }
+
+  get genres() {
+    let {movie} = this.args
+    return movie.genres.slice(0, 4)
+  }
 }
 

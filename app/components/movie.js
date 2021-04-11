3ed02cadb28816945acf922b428032f57f9b71a1
cherrybar <elenal@kmslh.com>
2021-04-12 00:20:08 +0300

    minor changes

--- app/components/movie.js
+++ app/components/movie.js
@@ -1,6 +1,3 @@
 import Component from '@glimmer/component';
-import { tracked } from "@glimmer/tracking";
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default class MovieComponent extends Component {}
